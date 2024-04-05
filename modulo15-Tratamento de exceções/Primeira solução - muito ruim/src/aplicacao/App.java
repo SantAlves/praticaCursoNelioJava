185de309e6eea47c29bc7d359f98f7e2fb77c0d2
Isaias Alves <isaias-alves-ds@hotmail.com>
2024-04-05 15:13:32 -0300

    Exercício demonstrando solução ruim. Tratando erros no programa principal.

--- modulo15-Tratamento de exceções/Primeira solução - muito ruim/src/aplicacao/App.java
+++ modulo15-Tratamento de exceções/Primeira solução - muito ruim/src/aplicacao/App.java
@@ -22,8 +22,29 @@ public class App {
         if(!deCheckout.after(deCheckin)){
             System.out.println("Data de Check-out deve ser depois de Data Check-in");
         }else{
+           
             Reserva reserva = new Reserva(numero, deCheckin, deCheckout);
             System.out.println("Reserva: " + reserva);
+
+            System.out.println();
+            System.out.println("Entre com os dados para atualizar a reserva: ");
+            System.out.println("Data de Check-in: (dd/MM/yyyy)");
+            deCheckin = confParaDataApp.parse(entrada.next());
+            System.out.println("Data de Check-out: (dd/MM/yyyy)");
+            deCheckout = confParaDataApp.parse(entrada.next());
+
+            Date agora = new Date();
+            if(!deCheckout.before(agora) || deCheckin.before(agora)){
+                System.out.println("Erro na reserva: atualização apenas para datas futuras.");
+            } else if(!deCheckout.after(deCheckout)){
+                System.out.println("Erro na reserva: datas de check-out devem ser depois da data de check-in.");
+            }else{
+                reserva.atualizacaoData(deCheckin, deCheckout);
+                System.out.println("Reserva: " + reserva);
+            }
+
         }
+
+        entrada.close();
     }
 }
